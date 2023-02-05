dc232f7cba42f8182edd4b3e1b410bce331629b5
Ron Ronaldo Ronalds <65684769+ronalds1979@users.noreply.github.com>
2023-02-05 21:25:21 +0100

    TPBot controlled by Elecfreaks joystick:bit via RF, including function to avoid get stuck

--- main.ts
+++ main.ts
@@ -1,13 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
     basic.showString("" + (receivedNumber))
     if (receivedNumber == 1) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 0.5)
+        Speed = 60
+        basic.showString("60")
     } else if (receivedNumber == 2) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Forward, 50, 2)
+        Speed = 70
+        basic.showString("70")
     } else if (receivedNumber == 3) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 2)
+        Speed = 80
+        basic.showString("80")
     } else if (receivedNumber == 4) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Right, 50, 0.5)
+        Speed = 100
+        basic.showString("100")
     }
 })
 radio.onReceivedValue(function (name, value) {
@@ -17,18 +21,115 @@ radio.onReceivedValue(function (name, value) {
         Y = value
     }
 })
+function Avoid () {
+    if (TPBot.sonarJudge(TPBot.Sonarjudge.Less, 30)) {
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Square,
+        400,
+        600,
+        255,
+        0,
+        100,
+        SoundExpressionEffect.Warble,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        TPBot.stopCar()
+        TPBot.headlightRGB(randint(0, 255), randint(0, 255), randint(0, 255))
+        basic.showIcon(IconNames.Surprised)
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, Speed, 1)
+        TPBot.setTravelTime(TPBot.DriveDirection.Left, Speed, 1)
+    }
+}
 let Y = 0
 let X = 0
+let Speed = 0
 basic.showIcon(IconNames.Heart)
 radio.setGroup(7)
+Speed = 50
+X = 460
+Y = 460
 basic.forever(function () {
     if ((X > 470 || X < 530) && Y > 980) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Forward, 50, 2)
+        TPBot.setTravelTime(TPBot.DriveDirection.Forward, Speed, 2)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        500,
+        500,
+        255,
+        0,
+        50,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+        Avoid()
     } else if ((X > 470 || X < 530) && Y < 40) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Backward, 50, 2)
+        TPBot.setTravelTime(TPBot.DriveDirection.Backward, Speed, 2)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        500,
+        500,
+        255,
+        0,
+        50,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            . . # . .
+            `)
+        Avoid()
     } else if (X > 980 && (Y > 470 || Y < 530)) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Left, 50, 0.5)
+        TPBot.setTravelTime(TPBot.DriveDirection.Left, Speed, 0.5)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        500,
+        500,
+        255,
+        0,
+        50,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            # # # # #
+            . # # . .
+            . . # . .
+            `)
+        Avoid()
     } else if (X < 40 && (Y > 470 || Y < 530)) {
-        TPBot.setTravelTime(TPBot.DriveDirection.Right, 50, 0.5)
+        TPBot.setTravelTime(TPBot.DriveDirection.Right, Speed, 0.5)
+        music.playSoundEffect(music.createSoundEffect(
+        WaveShape.Sine,
+        500,
+        500,
+        255,
+        0,
+        50,
+        SoundExpressionEffect.Vibrato,
+        InterpolationCurve.Linear
+        ), SoundExpressionPlayMode.UntilDone)
+        basic.showLeds(`
+            . . # . .
+            . . # # .
+            # # # # #
+            . . # # .
+            . . # . .
+            `)
+        Avoid()
+    } else {
+        TPBot.stopCar()
     }
 })
